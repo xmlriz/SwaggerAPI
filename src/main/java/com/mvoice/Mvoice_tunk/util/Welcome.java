d6cc67623813a9aac88738c4ae18d8d110d2736c
xmlriz <46624836+xmlriz@users.noreply.github.com>
2025-10-15 22:18:16 +0530

    this is command liner added

---
+++ src/main/java/com/mvoice/Mvoice_tunk/util/Welcome.java
@@ -0,0 +1,10 @@
+package com.mvoice.Mvoice_tunk.util;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class Welcome implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("\n\n\n Welcome to Spring boot !! \n\n\n");
+    }
+}
