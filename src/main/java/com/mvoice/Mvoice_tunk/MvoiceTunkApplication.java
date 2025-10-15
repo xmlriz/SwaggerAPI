a865f803c5de0893f8b2212b59e8ad26cee1c79a
xmlriz <46624836+xmlriz@users.noreply.github.com>
2025-10-15 21:33:45 +0530

    this is first

---
+++ src/main/java/com/mvoice/Mvoice_tunk/MvoiceTunkApplication.java
@@ -0,0 +1,13 @@
+package com.mvoice.Mvoice_tunk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvoiceTunkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvoiceTunkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mvoice/Mvoice_tunk/MvoiceTunkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvoice.Mvoice_tunk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvoiceTunkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
