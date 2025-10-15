36f63d2b34ffd1b0ee6a14880e86293bbccbcabc
xmlriz <46624836+xmlriz@users.noreply.github.com>
2025-10-15 22:21:37 +0530

    this is command liner added 1

--- src/main/java/com/mvoice/Mvoice_tunk/util/Welcome.java
+++ src/main/java/com/mvoice/Mvoice_tunk/util/Welcome.java
@@ -1,7 +1,9 @@
 package com.mvoice.Mvoice_tunk.util;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Welcome implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
